7a7a4213abc93ef2bfdac17f877ac2fceebbde5f
Luca Lusso <lussoluca@gmail.com>
2020-04-04 12:09:01 +0200

    Migliora il layout su mobile

--- themes/bootstrap/source/js/bootstrap.js
+++ themes/bootstrap/source/js/bootstrap.js
@@ -1,3 +1,11 @@
 import * as jquery from 'jquery/dist/jquery'
 import * as popper from 'popper.js/dist/popper'
 import * as collapse from 'bootstrap/js/dist/collapse'
+
+jquery(function () {
+  'use strict';
+
+  jquery('[data-toggle="offcanvas"]').on('click', function () {
+    jquery('.offcanvas-collapse').toggleClass('open')
+  })
+});
